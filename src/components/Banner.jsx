5a8a9f0337adfd56580755f4f4134bf387560557
romeokany187 <romeokany187@gmail.com>
2024-03-19 22:20:22 +0100

    corrections

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -26,9 +26,9 @@ const Banner = () => {
                     <p className='text-[#dce5e2]  text-[1.1rem] mb-[3rem]'>Chez <span className='text-green'>r<span>O</span>oy.creativo</span> Nous croyons en la puissance de la créativité pour transforùer des idées en réalisation exceptionelles</p>
                     <div className="flex gap-6">
                         <div className="">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler p-2 bg-secondary rounded-full icon-tabler-player-play" width="54" height="54" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler bg-green rounded-full p-2 icon-tabler-player-play-filled" width="54" height="54" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M7 4v16l13 -8z" />
+                                <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="white" />
                             </svg>
                         </div>
                         <div className="text-[#dce5e2] text-[1rem] font-medium ">
