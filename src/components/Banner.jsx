0c571c8611c0f13cc9fc851cf512a28ff0739e24
romeokany187 <romeokany187@gmail.com>
2024-03-19 21:05:17 +0100

    corrections

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
             <div className="" style={
                 {
                     width: '100vw',
-                    height: '78vh',
+                    height: '91vh',
                     marginTop: "4rem",
                     backgroundImage: `url(${banner})`,
                     backgroundRepeat: 'no-repeat',
